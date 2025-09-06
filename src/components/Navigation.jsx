9c9860c32c36e768b4bb19cd9639a33eb50c922a
hasanmubarok9 <hasanmubarok32@gmail.com>
2025-09-06 09:25:38 +0700

    init personal notes app starter

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+      <header>
+        <h1>Aplikasi Catatan</h1>
+        <nav>
+          <ul>
+            <li><a href="/">Arsip</a></li>
+          </ul>
+        </nav>
+      </header>
+  )
+}
+
+export default Navigation;
